0315727dd823a9365f9d7766377d1ab5747a6765
Christoffer Gustafsson <christoffer.gustafsson@dice.se>
2025-10-22 11:37:16 +0200

    .

--- wezterm.lua
+++ wezterm.lua
@@ -3,38 +3,34 @@ local wezterm = require 'wezterm'
 -- Set PowerShell as the default shell
 local default_prog = { "pwsh.exe" }
 
--- Solarized Light color scheme
+-- Solarized Light (modified) color scheme
 local solarized_light = {
-  foreground = "#586e75",
+  foreground = "#657B83",
   background = "#fdf6e3",
-
   cursor_fg = "#002b36",
   cursor_bg = "#657b83",
   cursor_border = "#657b83",
-
   selection_fg = "#586e75",
-  selection_bg = "#eee8d5",
-
+  selection_bg = "#FFFFFF",
   ansi = {
-    "#073642", -- black
+    "#002B36", -- black
     "#dc322f", -- red
     "#859900", -- green
     "#b58900", -- yellow
-    "#268bd2", -- blue
+    "#80CAFF", -- blue
     "#d33682", -- magenta
     "#2aa198", -- cyan
-    "#9e9885", -- white
+    "#7d796f", -- white
   },
-
   brights = {
-    "#002b36", -- bright black
+    "#073642", -- bright black
     "#cb4b16", -- bright red
     "#586e75", -- bright green
     "#657b83", -- bright yellow
     "#839496", -- bright blue
     "#6c71c4", -- bright magenta
     "#93a1a1", -- bright cyan
-    "#fdf6e3", -- bright white
+    "#57554E", -- bright white
   },
 }
 
